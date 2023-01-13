1cd7e9b174f9aff6d42fda44f62ae6e59c26a846
sweetapplepumpkinpie <sweetapplepumpkinpie@gmail.com>
2023-01-13 07:09:45 -0500

    add auto updater

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'node:os'
-import { join } from 'node:path'
+import { app, BrowserWindow, shell, ipcMain } from "electron"
+import { release } from "node:os"
+import { join } from "node:path"
 
 // The built directory structure
 //
@@ -12,22 +12,28 @@ import { join } from 'node:path'
 // ├─┬ dist
 // │ └── index.html    > Electron-Renderer
 //
-process.env.DIST_ELECTRON = join(__dirname, '..')
-process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
+process.env.DIST_ELECTRON = join(__dirname, "..")
+process.env.DIST = join(process.env.DIST_ELECTRON, "../dist")
 process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
+  ? join(process.env.DIST_ELECTRON, "../public")
   : process.env.DIST
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (release().startsWith("6.1")) app.disableHardwareAcceleration()
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === "win32") app.setAppUserModelId(app.getName())
 
 if (!app.requestSingleInstanceLock()) {
   app.quit()
   process.exit(0)
 }
+
+require("update-electron-app")({
+  repo: "https://github.com/sweetapplepumpkinpie/electron-vue.git",
+  updateInterval: "5 minutes",
+  logger: require("electron-log"),
+})
 
 // Remove electron security warnings
 // This warning only shows in development mode
@@ -36,14 +42,14 @@ if (!app.requestSingleInstanceLock()) {
 
 let win: BrowserWindow | null = null
 // Here, you can also use other preload
-const preload = join(__dirname, '../preload/index.js')
+const preload = join(__dirname, "../preload/index.js")
 const url = process.env.VITE_DEV_SERVER_URL
-const indexHtml = join(process.env.DIST, 'index.html')
+const indexHtml = join(process.env.DIST, "index.html")
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
-    icon: join(process.env.PUBLIC, 'favicon.ico'),
+    title: "Main window",
+    icon: join(process.env.PUBLIC, "favicon.ico"),
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -54,7 +60,8 @@ async function createWindow() {
     },
   })
 
-  if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // electron-vite-vue#298
     win.loadURL(url)
     // Open devTool if the app is not packaged
     win.webContents.openDevTools()
@@ -63,25 +70,25 @@ async function createWindow() {
   }
 
   // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString())
   })
 
   // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
+    if (url.startsWith("https:")) shell.openExternal(url)
+    return { action: "deny" }
   })
 }
 
 app.whenReady().then(createWindow)
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   win = null
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== "darwin") app.quit()
 })
 
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   if (win) {
     // Focus on the main window if the user tried to open another
     if (win.isMinimized()) win.restore()
@@ -89,7 +96,7 @@ app.on('second-instance', () => {
   }
 })
 
-app.on('activate', () => {
+app.on("activate", () => {
   const allWindows = BrowserWindow.getAllWindows()
   if (allWindows.length) {
     allWindows[0].focus()
@@ -99,7 +106,7 @@ app.on('activate', () => {
 })
 
 // New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
+ipcMain.handle("open-win", (_, arg) => {
   const childWindow = new BrowserWindow({
     webPreferences: {
       preload,
